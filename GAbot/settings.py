e3e167f197852d75c8853bf24dbeff3b88e7cc60
LDG-BBgod <padzz321@naver.com>
2022-07-01 01:05:21 +0900

    오류 확인 제거

--- GAbot/settings.py
+++ GAbot/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ielh!3pdu%)ahxk3&efj+_b2fr9&dkjvow9q#wlwwjpl+**4zu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.gabot.co.kr',
