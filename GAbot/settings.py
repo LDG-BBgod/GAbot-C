da790abf623503febc88fbfafb54783fda726ba3
LDG-BBgod <padzz321@naver.com>
2022-07-01 00:56:43 +0900

    장고 오류 확인

--- GAbot/settings.py
+++ GAbot/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ielh!3pdu%)ahxk3&efj+_b2fr9&dkjvow9q#wlwwjpl+**4zu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.gabot.co.kr',
